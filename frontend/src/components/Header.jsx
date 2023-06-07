f36872105eef87252bf02529625b833a4b02fcb6
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-07 13:51:29 +0200

    update ui✅

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -11,7 +11,10 @@ const Header = () => {
   return (
     <motion.div
       className="flex-1 text-center font-secondary lg:text-left"
-      style={{ textAlign: "center" }}
+      style={{
+        textAlign: "center",
+        fontFamily: "Courier New, monospace",
+      }}
     >
       <motion.h1
         variants={fadeIn("up", 0.3)}
@@ -19,7 +22,6 @@ const Header = () => {
         animate="show"
         className="text-[55px] font-bold leading-[0.8] lg:text-[110px]"
         style={{
-          fontFamily: "Courier New, monospace",
           fontSize: "150px",
           fontWeight: "bold",
           color: "#FFFFFF",
@@ -34,7 +36,7 @@ const Header = () => {
         className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]"
         style={{ fontFamily: "Courier New, monospace" }}
       >
-        <span className="text-white mr-10" style={{ fontSize: "4rem" }}>
+        <span className="text-white mr-10" style={{ fontSize: "3rem" }}>
           🤩
         </span>
         <TypeAnimation
@@ -50,16 +52,9 @@ const Header = () => {
           className="text-accent"
           wrapper="span"
           repeat={Infinity}
-          style={{ fontSize: "4rem" }} // Adjust the font size here
+          style={{ fontSize: "3rem" }}
         />
       </motion.div>
-      <motion.p
-        variants={fadeIn("up", 0.5)}
-        initial="hidden"
-        animate="show"
-        className="mb-8 max-w-lg mx-auto lg:mg-0"
-        style={{ fontFamily: "Courier New, monospace" }}
-      ></motion.p>
     </motion.div>
   );
 };
