14ec0aa2a9679f4b99825b5a3a1535c990ddc53e
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-07 12:07:30 +0200

    update ui✅

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Header() {
   const headerStyle = {
-    fontFamily: "Roboto Mono, monospace", // Replace with the appropriate font for your hackathon project
+    fontFamily: "Courier New, monospace",
     fontSize: "150px",
     fontWeight: "bold",
     color: "#FFFFFF",
-    backgroundColor: "#5A10", // You can use any color or gradient for the background
+    backgroundColor: "#5A10",
     padding: "100px",
     textAlign: "center",
   };
