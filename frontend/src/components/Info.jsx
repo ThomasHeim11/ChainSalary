2058f326cade11353667f1b0239112e72210ecf5
ThomasHeim11 <wearedreamin@gmail.com>
2023-06-08 17:26:32 +0200

    update ui✅

--- frontend/src/components/Info.jsx
+++ frontend/src/components/Info.jsx
@@ -5,7 +5,7 @@ function Info() {
     background: "rgba(0, 0, 128, 0.19)",
     color: "#FFFFFF",
     padding: "20px",
-    borderRadius: "85px", // Added border radius
+    borderRadius: "85px",
   };
 
   return (
@@ -14,10 +14,17 @@ function Info() {
       <ol className="list-decimal pl-8 mb-6">
         <li className="mb-2">Install Metamask to use the app🪄</li>
         <li className="mb-2">Connect your Wallet (Ethereum Sepolia)🎊</li>
-        <li className="mb-2">Input details on the employee (Biweekly payment schedule)👩‍🚀</li>
+        <li className="mb-2">
+          Input details on the employee (Biweekly payment schedule)👩‍🚀
+        </li>
         <li className="mb-2">Deploy the Salary Automation contract✅</li>
-        <li className="mb-2">Register the contract with Chainlink Keeper Registry (requires funding with LINK)🔗</li>
-        <li className="mb-2">Enjoy automated salary payments for your employee🎉</li>
+        <li className="mb-2">
+          Register the contract with Chainlink Keeper Registry (requires funding
+          with LINK)🔗
+        </li>
+        <li className="mb-2">
+          Enjoy automated salary payments for your employee🎉
+        </li>
       </ol>
     </div>
   );
